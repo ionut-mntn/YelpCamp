f4625660681b100dbd0415e9d4b6736a3b339d48
Ionut Muntean <ionut.muntean@linkgraph.io>
2022-11-08 22:06:18 +0200

    finish adding maps

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapboxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
@@ -12,9 +15,21 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
     // console.log('intra unde trebuie POST!')
-
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    // console.log('hey!');
+    // console.log(geoData);
+    // console.log('hey 2!');
+    // console.log(geoData.body.features);
+    // console.log('hey 3!');
+    // console.log(geoData.body.features[0].geometry.coordinates);
+    // res.send('OK!!!')
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+
     campground.author = req.user._id;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     await campground.save();
@@ -61,8 +76,8 @@ module.exports.updateCampground = async (req, res) => {  // mare atentie la ches
     // req.flash('succes', 'Successfully updated campground!')  / grija mari la greseli de genul!!
     await campground.save();
 
-    if(req.body.deleteImages){
-        for(let fileName of req.body.deleteImages){
+    if (req.body.deleteImages) {
+        for (let fileName of req.body.deleteImages) {
             await cloudinary.uploader.destroy(fileName);
         }
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            // YOUR USER ID
             author: '6368ce99180d564696e85bbf',  // all belong to Tim rn
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
@@ -48,7 +49,11 @@ const seedDB = async () => {
                 }
             ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            }
         })
         await camp.save();
     }
